555aa7c9821c30ce6c2c5677cb5c853752eb2256
James Phillips <james.phillips@bryanuniversity.edu>
2019-10-31 15:17:22 -0500

    Added Loops & Arrays

--- Week 3/Loops And Arrays/index.js
+++ Week 3/Loops And Arrays/index.js
@@ -1,7 +1,7 @@
 // 1
-// var numOfAnswers = 0;
-// var answers = [ "stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer", "computer" ];
-// console.log(answers.toString().match(/computer/g).length);
+var numOfAnswers = 0;
+var answers = [ "stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer", "computer" ];
+console.log(answers.toString().match(/computer/g).length);
 
 2
 var peopleWhoWantToSeeMadMaxFuryRoad = [{
@@ -25,46 +25,46 @@ var peopleWhoWantToSeeMadMaxFuryRoad = [{
     age: 4,
     gender: "female"
 }]
-// for(let x = 0; x < peopleWhoWantToSeeMadMaxFuryRoad.length; x++) {
-//     if(peopleWhoWantToSeeMadMaxFuryRoad[x].age >= 18){
-//        console.log(peopleWhoWantToSeeMadMaxFuryRoad[x].name + " is old enough");
-//     }else {
-//        console.log(peopleWhoWantToSeeMadMaxFuryRoad[x].name + " is not old enough");
-//     } 
-// }
+for(let x = 0; x < peopleWhoWantToSeeMadMaxFuryRoad.length; x++) {
+    if(peopleWhoWantToSeeMadMaxFuryRoad[x].age >= 18){
+       console.log(peopleWhoWantToSeeMadMaxFuryRoad[x].name + " is old enough");
+    }else {
+       console.log(peopleWhoWantToSeeMadMaxFuryRoad[x].name + " is not old enough");
+    } 
+}
 
 // 3
-// for(let x = 0; x < peopleWhoWantToSeeMadMaxFuryRoad.length; x++) {
-//     if(peopleWhoWantToSeeMadMaxFuryRoad[x].age >= 18){
-//        console.log(peopleWhoWantToSeeMadMaxFuryRoad[x].name + " is old enough to see Mad Max.");
-//     }else {
-//        console.log(peopleWhoWantToSeeMadMaxFuryRoad[x].name + " is not old enough to see Mad Max.");
-//     } 
-// }
+for(let x = 0; x < peopleWhoWantToSeeMadMaxFuryRoad.length; x++) {
+    if(peopleWhoWantToSeeMadMaxFuryRoad[x].age >= 18){
+       console.log(peopleWhoWantToSeeMadMaxFuryRoad[x].name + " is old enough to see Mad Max.");
+    }else {
+       console.log(peopleWhoWantToSeeMadMaxFuryRoad[x].name + " is not old enough to see Mad Max.");
+    } 
+}
 
 // 4
-// for(let x = 0; x < peopleWhoWantToSeeMadMaxFuryRoad.length; x++) {
-//     if(peopleWhoWantToSeeMadMaxFuryRoad[x].age >= 18){
+for(let x = 0; x < peopleWhoWantToSeeMadMaxFuryRoad.length; x++) {
+    if(peopleWhoWantToSeeMadMaxFuryRoad[x].age >= 18){
 
-// if (peopleWhoWantToSeeMadMaxFuryRoad[x].gender == "male"){
-// console.log(peopleWhoWantToSeeMadMaxFuryRoad[x].name + " is old enough to see Mad Max Fury Road, let HIM in.");
-// } else if (peopleWhoWantToSeeMadMaxFuryRoad[x].gender == "female"){
-// console.log(peopleWhoWantToSeeMadMaxFuryRoad[x].name + " is old enough to see Mad Max Fury Road, let HER in.");
-// }
-// } else if 
-// (peopleWhoWantToSeeMadMaxFuryRoad[x].gender == "male"){
-// console.log(peopleWhoWantToSeeMadMaxFuryRoad[x].name + " is not old enough to see Mad Max Fury Road, don't let HIM in.");
-// } else {
-// (peopleWhoWantToSeeMadMaxFuryRoad[x].gender == "female")
-// console.log(peopleWhoWantToSeeMadMaxFuryRoad[x].name + " is not old enough to see Mad Max Fury Road, don't let HER in.");
-// }}
+if (peopleWhoWantToSeeMadMaxFuryRoad[x].gender == "male"){
+console.log(peopleWhoWantToSeeMadMaxFuryRoad[x].name + " is old enough to see Mad Max Fury Road, let HIM in.");
+} else if (peopleWhoWantToSeeMadMaxFuryRoad[x].gender == "female"){
+console.log(peopleWhoWantToSeeMadMaxFuryRoad[x].name + " is old enough to see Mad Max Fury Road, let HER in.");
+}
+} else if 
+(peopleWhoWantToSeeMadMaxFuryRoad[x].gender == "male"){
+console.log(peopleWhoWantToSeeMadMaxFuryRoad[x].name + " is not old enough to see Mad Max Fury Road, don't let HIM in.");
+} else {
+(peopleWhoWantToSeeMadMaxFuryRoad[x].gender == "female")
+console.log(peopleWhoWantToSeeMadMaxFuryRoad[x].name + " is not old enough to see Mad Max Fury Road, don't let HER in.");
+}}
 
 // 5
-// for (var i = 0; i < 101; i++) {
-//     if ((i % 2) === 0) {
-//         console.log(i + ' ' + "is even")
-//     } else {
-//         console.log(i + ' ' + "is odd")
-//     }
-// } 
+for (var i = 0; i < 101; i++) {
+    if ((i % 2) === 0) {
+        console.log(i + ' ' + "is even")
+    } else {
+        console.log(i + ' ' + "is odd")
+    }
+} 
 
