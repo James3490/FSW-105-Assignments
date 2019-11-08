c1c9be18c2448275c77bed6516ee513f70b4b996
James Phillips <james.phillips@bryanuniversity.edu>
2019-11-08 14:06:54 -0600

    Added Calculator JS

--- Week 4/JavaScript Calculator/calculator.js
+++ Week 4/JavaScript Calculator/calculator.js
@@ -7,19 +7,19 @@ var num1 = readlineSync.question ('What is your first number?');
 
 var num2 = readlineSync.question ('What is your second number?');
 
-var operation = readlineSync.question ('Please enter the operation to perform: + - * /')
+var operation = readlineSync.question ('Please enter the operation to perform: + - * /: ')
 
 switch(operation) {
     case "+":
-    console.log((userName), "The result is: ",(num1) + (num2));
+        console.log('The result is: ',  Number(num1) + Number(num2));
     break;
     case "-":
-    console.log((userName), "The result is: ",  (num1) - (num2));
+    console.log('The result is: ',  Number(num1) - Number(num2));
     break;
     case "*":
-    console.log((userName), "The result is: ",(num1) * (num2));
+    console.log('The result is: ',Number(num1) * Number(num2));
     break;
     case "/":
-    console.log((userName), "The result is: ",(num1) / (num2));
+    console.log('The result is: ',Number(num1) / Number(num2));
     break;
 }
